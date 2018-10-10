c1fc967fcaa9194a8d96b82eea6d243fdee16f5b
Hannah Scovill <hnsvill@gmail.com>
2018-10-10 12:42:02 -0700

    finished interview submission

--- rna_transcription.py
+++ rna_transcription.py
@@ -1,2 +1,19 @@
 def to_rna(dna_strand):
-    pass
+    # convert string to list
+    # run each item in the new list through the dictionary to transcribe
+    # convert the transcribed list back into a string
+    # return the new string
+
+    # Transcription dictionary: the key is the DNA letter, the RNA is the value
+    transcription = {'G':'C', 'C':'G', 'T':'A', 'A':'U'}
+
+    broken = list(dna_strand) # convert string to list
+    transcribed = [] #initialize the list
+
+    for dna in broken:
+        transcribed.append(transcription[dna])
+
+    return ("".join(transcribed))
+
+if __name__ == '__main__':
+    to_rna("GC")
